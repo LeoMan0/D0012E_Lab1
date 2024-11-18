4db0c89aa0375646f2abdcef696363c1a0eb67fb
LeoMan0 <leoman3628@gmail.com>
2024-11-18 15:48:40 +0100

    Sort mostly working

--- sort.py
+++ sort.py
@@ -1,2 +1,27 @@
 from stack import Stack
 
+class Sort:
+    def __init__(self):
+        pass
+
+    def sortStack(self, inputStack):
+        tempStack = Stack()
+        #Whil is not empty
+        while not inputStack.isEmpty():
+            rightHand = inputStack.pop()
+
+            if tempStack.isEmpty():
+                tempStack.push(rightHand)
+            
+            else:
+                leftHand = tempStack.pop()
+                #tempStack.push(leftHand)
+
+                while not tempStack.isEmpty() and leftHand >= rightHand:
+                    inputStack.push(leftHand)
+                    leftHand = tempStack.pop()
+
+                tempStack.push(leftHand)
+                tempStack.push(rightHand)
+
+        return tempStack
